8300bffd3839ddf0ec5fa248a0c5bed3f7c21699
Luiz Branco <me@luizbranco.com>
2013-11-29 07:10:25 -0200

    Add grunt task to run eunit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    regarde: {
+      js: {
+        files: ["src/**/*.erl"],
+        tasks: ["shell:test"]
+      }
+    },
+    shell: {
+      test: {
+        command: "rebar eunit",
+        options: {
+          stdout: true,
+          stderr: true
+        }
+      }
+    }
+  });
+
+  // Tasks
+  grunt.registerTask("default", "eunit test", function () {
+    var tasks = ["regarde"];
+    grunt.option("force", true);
+    grunt.task.run(tasks);
+  });
+  grunt.registerTask("test", ["shell:test"]);
+
+  // Load Dependencies
+  grunt.loadNpmTasks("grunt-regarde");
+  grunt.loadNpmTasks("grunt-shell");
+  grunt.loadNpmTasks("grunt-notify");
+
+};
