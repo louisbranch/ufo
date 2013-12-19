21ad18312cc9580a4d4ce04e7df69b8ddde72824
Luiz Branco <me@luizbranco.com>
2013-12-18 23:46:55 -0200

    Refactor distribute hand

--- src/alienation_player.erl
+++ src/alienation_player.erl
@@ -5,38 +5,38 @@
 %% @doc Create new player
 -spec new(binary()) -> #player{}.
 new(Name) ->
-  #player{name=Name}.
+    #player{name=Name}.
 
 %% @doc Create and add player to a players list
 -spec add([#player{}], binary()) -> [#player{}].
 add(Players, Name) ->
-  [new(Name)|Players].
+    [new(Name)|Players].
 
 %% @doc Remove player using the name from a players list
 -spec remove([#player{}], binary()) -> [#player{}].
 remove(Players, Name) ->
- lists:keydelete(Name, #player.name, Players).
+    lists:keydelete(Name, #player.name, Players).
 
 %% @doc Get player hand
 -spec hand(#player{}) -> [tuple()].
 hand(Player) ->
-  Player#player.hand.
+    Player#player.hand.
 
 %% @doc Set player hand
 -spec hand(#player{}, [tuple()]) -> #player{}.
 hand(Player, Hand) ->
-  Player#player{hand=Hand}.
+    Player#player{hand=Hand}.
 
 %% @doc Get player name
 -spec name(#player{}) -> binary().
 name(Player) ->
-  Player#player.name.
+    Player#player.name.
 
 %% @doc Distribute a hand of cards for each player
 -spec distribute_hands([#player{}], [tuple()]) -> [#player{}].
 distribute_hands(Players, Hands) ->
-  {PlayersWithHands, _} = lists:foldl(fun (Player, {Ps, [Hand|Hs]}) ->
-    {[hand(Player,Hand)|Ps], Hs}
-    end, {[], Hands}, Players),
-  PlayersWithHands.
+    {PlayersWithHands, _} = lists:foldl(fun distribute/2, {[], Hands}, Players),
+    PlayersWithHands.
 
+distribute(Player, {Players, [Hand|Hands]}) ->
+    {[hand(Player,Hand)|Players], Hands}.
