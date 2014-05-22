d1f3c0256828f3a0c88fa59566a8eeb377be7420
Luiz Branco <me@luizbranco.com>
2014-05-22 09:14:35 -0300

    Update docs

---
+++ src/ufo_phase.erl
@@ -0,0 +1,22 @@
+-module(ufo_phase).
+%%
+%%-export([initial_attack/4]).
+
+%% @doc Attack the first 9 cities, adding 3, 2 and 1 aliens in sequence
+%%-spec initial_attack([ufo_city:city()], ufo_alien:pool(), [ufo_card:card()],
+                     %%[ufo_card:card()]) ->
+    %%{ok, [ufo:city()], ufo_alien:pool(), [ufo_card:card()], [ufo_card:card()]}.
+%%initial_attack(Cities, Pool, AlienDeck, DiscardPile) ->
+    %%{Cards, NewDeck, NewPile} = draw_city_cards(AlienDeck, DiscardPile, 9),
+
+
+
+%%draw_city_cards(Deck, Pile, N) -> draw_city_cards(N, []).
+
+%%draw_city_cards(Deck, Pile, 0, Cards) ->
+    %%{lists:reverse(Cards), Deck, Pile};
+%%draw_city_cards(Deck, Pile, N, Cards) ->
+    %%{Card, NewDeck, NewPile} = ufo_card:reveal(Deck, Pile),
+    %%draw_city_cards(NewDeck, NewPile, N - 1, [Card|Cards]).
+
+
